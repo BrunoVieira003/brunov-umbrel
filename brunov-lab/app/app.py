384dd9288e740cd1b6b58f9f561bde12347afe05
BrunoVieira003 <brunosilverio01@gmail.com>
2025-06-14 18:12:50 -0300

    test

--- brunov-lab/app/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, jsonify
-app = Flask(__name__)
-
-@app.route('/info')
-def info():
-    return jsonify(status="ok", message="test info")
-
-app.run(host='0.0.0.0', port=5000)
